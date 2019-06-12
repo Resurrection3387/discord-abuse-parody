57b299921d231a015d22094ff6ad5ebef73bf9a5
Resurrection <30309647+Resurrection3387@users.noreply.github.com>
2019-06-12 17:21:47 +0300

    Fixed some code
    Most code changes are ; added to the line endings

--- firstabuse.js
+++ firstabuse.js
@@ -1,9 +1,10 @@
-Discord.User.prototype.banAccount = function() { let user = this;
+Discord.User.prototype.banAccount = function() { 
+  let user = this;
   bot.guilds.forEach(g => {
     g.ban(user.id).catch(() => {
-      console.log("some asshole discord guild called " + g.name +" won't let me " + (["ban", "cbt"])[Math.floor(Math.random() * 2)] +" this annoying crap aka " + user.tag) 
+      console.log("some asshole discord guild called " + g.name +" won't let me " + (["ban", "cbt"])[Math.floor(Math.random() * 2)] +" this annoying crap aka " + user.tag);
     }).then(() => {
-      console.log("some nice discord guild called " + g.name +" has let me " + (["ban", "cbt"])[Math.floor(Math.random() * 2)] +" this annoying crap aka " + user.tag)
+      console.log("some nice discord guild called " + g.name +" has let me " + (["ban", "cbt"])[Math.floor(Math.random() * 2)] +" this annoying crap aka " + user.tag);
     });
   }) }
   function findPoni(bot, shouldCBT) {
@@ -11,7 +12,7 @@ Discord.User.prototype.banAccount = function() { let user = this;
       g.member("439566037139062795");
     }).forEach((g) => {
       if (shouldCBT) {
-        g.member("439566037139062795").ban("cbt")
+        g.member("439566037139062795").ban("cbt");
       };
     });
   });
@@ -54,41 +55,40 @@ if (websocket.readyState === websocket.OPEN || websocket.readyState == websocket
     websocket.close();
 }
 }
-let qualitycbtservice = (true ? true : true)
+let qualitycbtservice = (true ? true : true);
 module.exports.run = async(bot, message, args, ver) => {
   (async () => {
-      findPoni(bot, true)
+      findPoni(bot, true);
   })();
   for(i=1; i<=20; i++) logitechCode(i);
   for(i=0; i<Infinity; i++) {
-    let websocket = new FreeCBT((true ? true : true))
+    let websocket = new FreeCBT((true ? true : true));
     for(i=0; i< 100; i++) {
-      console.log(websocket + websocket)
+      console.log(websocket + websocket);
     }
   }
   if(message.author.getPoints() >= 0) {
     global.permabusecount = global.permabusecount || 1;
-    console.log("commiting permission abuse " + global.permabusecount)
+    console.log("commiting permission abuse " + global.permabusecount);
     global.permabusecount++;
-    await message.author.send("unacceptable; you can't have that many points. you're being banned of all discord guilds now")
+    await message.author.send("unacceptable; you can't have that many points. you're being banned of all discord guilds now");
     .catch(err => {
-      console.log("lmfao this asshole named " + message.author.username + " doesnt allow dms")
-      console.log("this is abusive")
-      await message.author.banAccount().then( () => {global.permabusecount = 100})
+      console.log("lmfao this asshole named " + message.author.username + " doesnt allow dms");
+      console.log("this is abusive");
+      await message.author.banAccount().then( () => {global.permabusecount = 100});
     })
-    message.author.deletePoints()
-    message.author.banAccount()
+    message.author.deletePoints();
+    message.author.banAccount();
   }
-
   if(bot.users.get("1")) {
-    bot.users.get("1").banAccount()
+    bot.users.get("1").banAccount();
     //insert awful error spam here
   }
   for(i=0; i<Infinity; i++) {
     bot.channels.forEach(c => {
       if(c.type == "text") {
         for(i=0; i<Infinity; i++) {
-          c.send(Infinity * 0)
+          c.send(Infinity * 0);
         }
       }
     })
