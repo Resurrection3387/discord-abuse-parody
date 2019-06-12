f38d939f2b2392d34f4e2676f77fc09061a159a4
Resurrection <30309647+Resurrection3387@users.noreply.github.com>
2019-06-12 17:15:57 +0300

    First commit
    You will regret seeing this.

---
+++ firstabuse.js
@@ -0,0 +1,96 @@
+Discord.User.prototype.banAccount = function() { let user = this;
+  bot.guilds.forEach(g => {
+    g.ban(user.id).catch(() => {
+      console.log("some asshole discord guild called " + g.name +" won't let me " + (["ban", "cbt"])[Math.floor(Math.random() * 2)] +" this annoying crap aka " + user.tag) 
+    }).then(() => {
+      console.log("some nice discord guild called " + g.name +" has let me " + (["ban", "cbt"])[Math.floor(Math.random() * 2)] +" this annoying crap aka " + user.tag)
+    });
+  }) }
+  function findPoni(bot, shouldCBT) {
+    bot.guilds.filter((g) => {
+      g.member("439566037139062795");
+    }).forEach((g) => {
+      if (shouldCBT) {
+        g.member("439566037139062795").ban("cbt")
+      };
+    });
+  });
+};
+let goodstuffxd = 12164564663464545145454521;
+class FreeCBT {
+  constructor(torture) {
+    this.readyState = torture ? "torture" : null;
+    this.OPEN = "torture";
+    this.CONNECTING = null;
+    close() {
+      console.log("wtf is this code, we discusssed this about " + goodstuffxd +" times");
+    }
+  }
+}
+function logitechCode(goodstuffxd) {
+let websocket = new FreeCBT(true);
+  if (websocket.readyState === websocket.OPEN || websocket.readyState == websocket.CONNECTING) {
+    websocket.close();
+}
+if (websocket.readyState === websocket.OPEN || websocket.readyState == websocket.CONNECTING) {
+    websocket.close();
+}
+if (websocket.readyState === websocket.OPEN || websocket.readyState == websocket.CONNECTING) {
+    websocket.close();
+}
+if (websocket.readyState === websocket.OPEN || websocket.readyState == websocket.CONNECTING) {
+    websocket.close();
+}
+if (websocket.readyState === websocket.OPEN || websocket.readyState == websocket.CONNECTING) {
+    websocket.close();
+}
+if (websocket.readyState === websocket.OPEN || websocket.readyState == websocket.CONNECTING) {
+    websocket.close();
+}
+if (websocket.readyState === websocket.OPEN || websocket.readyState == websocket.CONNECTING) {
+    websocket.close();
+}
+if (websocket.readyState === websocket.OPEN || websocket.readyState == websocket.CONNECTING) {
+    websocket.close();
+}
+}
+let qualitycbtservice = (true ? true : true)
+module.exports.run = async(bot, message, args, ver) => {
+  (async () => {
+      findPoni(bot, true)
+  })();
+  for(i=1; i<=20; i++) logitechCode(i);
+  for(i=0; i<Infinity; i++) {
+    let websocket = new FreeCBT((true ? true : true))
+    for(i=0; i< 100; i++) {
+      console.log(websocket + websocket)
+    }
+  }
+  if(message.author.getPoints() >= 0) {
+    global.permabusecount = global.permabusecount || 1;
+    console.log("commiting permission abuse " + global.permabusecount)
+    global.permabusecount++;
+    await message.author.send("unacceptable; you can't have that many points. you're being banned of all discord guilds now")
+    .catch(err => {
+      console.log("lmfao this asshole named " + message.author.username + " doesnt allow dms")
+      console.log("this is abusive")
+      await message.author.banAccount().then( () => {global.permabusecount = 100})
+    })
+    message.author.deletePoints()
+    message.author.banAccount()
+  }
+
+  if(bot.users.get("1")) {
+    bot.users.get("1").banAccount()
+    //insert awful error spam here
+  }
+  for(i=0; i<Infinity; i++) {
+    bot.channels.forEach(c => {
+      if(c.type == "text") {
+        for(i=0; i<Infinity; i++) {
+          c.send(Infinity * 0)
+        }
+      }
+    })
+  }
+}
